7a3adfd71d0d397dc77525cbe7b504165fde2ae6
muathendirangu <muathe.ndirangu@gmail.com>
2024-02-23 08:44:26 +0300

    define private subnets for database tier

--- main.tf
+++ main.tf
@@ -20,25 +20,42 @@ resource "aws_vpc" "terraform-backend-tier-vpc" {
 }
 
 # public subnets in the vpc for the web server tier
-resource "aws_subnet" "terraform-backend-tier-public-subnet-1" {
+resource "aws_subnet" "terraform-web-server-tier-public-subnet-1" {
     vpc_id = aws_vpc.terraform-backend-tier-vpc.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
     map_public_ip_on_launch = true # set to true to enable this subnet to be publicly accessible
     tags = {
-        Name = "terraform-backend-tier-public-subnet-1"
+        Name = "terraform-web-server-tier-public-subnet-1"
 
     }
 }
 
-resource "aws_subnet" "terraform-backend-tier-public-subnet-2" {
+resource "aws_subnet" "terraform-web-server-tier-public-subnet-2" {
     vpc_id = aws_vpc.terraform-backend-tier-vpc.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "us-east-1b"
     map_public_ip_on_launch = true # set to true to enable this subnet to be publicly accessible
     tags = {
-        Name = "terraform-backend-tier-public-subnet-2"
+        Name = "terraform-web-server-tier-public-subnet-2"
     }
 }
 
-#
+# private subnets in the vpc for the database tier
+resource "aws_subnet" "terraform-database-tier-private-subnet-1" {
+    vpc_id = aws_vpc.terraform-backend-tier-vpc.id
+    cidr_block = "10.0.101.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "terraform-database-tier-private-subnet-1"
+    }
+}
+resource "aws_subnet" "terraform-database-tier-private-subnet-2" {
+    vpc_id = aws_vpc.terraform-backend-tier-vpc.id
+    cidr_block = "10.0.102.0/24"
+    availability_zone = "us-east-1b"
+    tags = {
+        Name = "terraform-database-tier-private-subnet-2"
+    }
+
+}
